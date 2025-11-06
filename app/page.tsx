ad0ed8d386ee65e436120c6d8152904cad2eff0c
Veeno <77930262+Veenoway@users.noreply.github.com>
2025-11-06 15:27:08 +0100

    fix: typo

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,6 @@ export default function Home() {
     txError,
     txHash,
     totalSupply,
-    maxSupply,
   } = useMint();
 
   const [showSuccess, setShowSuccess] = useState(false);
@@ -237,7 +236,7 @@ export default function Home() {
                     d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                   />
                 </svg>
-                {isPending ? "Confirming..." : "Minting..."}
+                {isPending ? "Confirming..." : "Loading..."}
               </span>
             ) : (
               "Mint Basie"
